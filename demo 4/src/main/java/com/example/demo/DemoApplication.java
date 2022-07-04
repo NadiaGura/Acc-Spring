0d6e6d164ca5208b8bc43b91fd482370daa46bb5
NadiaGura <gura.nadezda@gmail.com>
2022-07-04 11:50:58 +0300

    init

--- demo 4/src/main/java/com/example/demo/DemoApplication.java
+++ demo 4/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+//source: spring.io tutorial
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController   // annotation to tell Spring that code is an endpoint that shuld be available ot he Web
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@GetMapping("/hello")  // use hello() to answer requests that get sent to localhost
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) { //hello() to take a param name and then to combine it with the "World"
 		return String.format("Hello %s!", name);
 	}
 }
